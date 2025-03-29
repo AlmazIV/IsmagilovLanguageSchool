c023dc00a10faad551993a666022082e9443cf54
AlmazIV <almaz.ismagilov2018@gmail.com>
2025-03-29 11:17:39 +0300

    Добавьте файлы проекта.

---
+++ Исмагилов_ЯШ/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Исмагилов_ЯШ
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; } 
+    }
+}
